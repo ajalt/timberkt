33777e11a2094f093a8cc22855f3a6ccc7426514
AJ Alt <aj@alt>
2016-06-29 03:20:25 +0100

    Add top level functions.

--- library/src/main/kotlin/com/github/ajalt/timberkt/TimberKt.kt
+++ library/src/main/kotlin/com/github/ajalt/timberkt/TimberKt.kt
@@ -121,6 +121,46 @@ inline fun Timber.Tree.wtf(noinline message: () -> String) = wtf("%s", LazyStrin
 /** Log an assert exception and a message that will be evaluated lazily when the message is printed */
 inline fun Timber.Tree.wtf(t: Throwable, noinline message: () -> String) = wtf(t, "%s", LazyString(message))
 
+//
+// Plain functions
+//
+
+/** Log a verbose message that will be evaluated lazily when the message is printed */
+inline fun v(noinline message: () -> String) = Timber.v("%s", LazyString(message))
+
+/** Log a verbose exception and a message that will be evaluated lazily when the message is printed */
+inline fun v(t: Throwable, noinline message: () -> String) = Timber.v(t, "%s", LazyString(message))
+
+/** Log a debug message that will be evaluated lazily when the message is printed */
+inline fun d(noinline message: () -> String) = Timber.d("%s", LazyString(message))
+
+/** Log a debug exception and a message that will be evaluated lazily when the message is printed */
+inline fun d(t: Throwable, noinline message: () -> String) = Timber.d(t, "%s", LazyString(message))
+
+/** Log an info message that will be evaluated lazily when the message is printed */
+inline fun i(noinline message: () -> String) = Timber.i("%s", LazyString(message))
+
+/** Log an info exception and a message that will be evaluated lazily when the message is printed */
+inline fun i(t: Throwable, noinline message: () -> String) = Timber.i(t, "%s", LazyString(message))
+
+/** Log a warning message that will be evaluated lazily when the message is printed */
+inline fun w(noinline message: () -> String) = Timber.w("%s", LazyString(message))
+
+/** Log a warning exception and a message that will be evaluated lazily when the message is printed */
+inline fun w(t: Throwable, noinline message: () -> String) = Timber.w(t, "%s", LazyString(message))
+
+/** Log an error message that will be evaluated lazily when the message is printed */
+inline fun e(noinline message: () -> String) = Timber.e("%s", LazyString(message))
+
+/** Log an error exception and a message that will be evaluated lazily when the message is printed */
+inline fun e(t: Throwable, noinline message: () -> String) = Timber.e(t, "%s", LazyString(message))
+
+/** Log an assert message that will be evaluated lazily when the message is printed */
+inline fun wtf(noinline message: () -> String) = Timber.wtf("%s", LazyString(message))
+
+/** Log an assert exception and a message that will be evaluated lazily when the message is printed */
+inline fun wtf(t: Throwable, noinline message: () -> String) = Timber.wtf(t, "%s", LazyString(message))
+
 class LazyString(val initializer: () -> String) {
     val string: String by lazy(initializer)
     override fun toString() = string
