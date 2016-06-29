592bcebad280865df9f44a45ca2ac1e31b9b14fb
AJ Alt <aj@alt>
2016-06-29 03:35:27 +0100

    Add more examples to sample activity.

--- sample/src/main/kotlin/com/github/ajalt/timberkt/sample/MainActivity.kt
+++ sample/src/main/kotlin/com/github/ajalt/timberkt/sample/MainActivity.kt
@@ -2,7 +2,8 @@ package com.github.ajalt.timberkt.sample
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import com.github.ajalt.timberkt.Timber
+import com.github.ajalt.timberkt.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,10 +11,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         var i = 0
-        Timber.d {"log ${i++}"}
-        Timber.i {"log ${i++}"}
-        Timber.w {"log ${i++}"}
-        Timber.e {"log ${i++}"}
-        Timber.wtf {"log ${i++}"}
+
+        Timber.d { "log ${i++}" }
+        Timber.i { "log ${i++}" }
+        Timber.w { "log ${i++}" }
+        Timber.e { "log ${i++}" }
+        Timber.wtf { "log ${i++}" }
+
+        Timber.tag("Custom tag").d { "log ${i++}" }
+        Timber.tag("Custom tag").i { "log ${i++}" }
+        Timber.tag("Custom tag").w { "log ${i++}" }
+        Timber.tag("Custom tag").e { "log ${i++}" }
+        Timber.tag("Custom tag").wtf { "log ${i++}" }
+
+        d { "log ${i++}" }
+        i { "log ${i++}" }
+        w { "log ${i++}" }
+        e { "log ${i++}" }
+        wtf { "log ${i++}" }
     }
 }
